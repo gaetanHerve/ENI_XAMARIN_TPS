9ffd52442afa1cced1da398a9c8bb87b37d3e7e0
Gaëtan <gaetan.herve@lilo.org>
2020-06-25 13:24:16 +0200

    M07_TP04

---
+++ XAMARIN_TPS/M07_TP04/M07_TP04/Services/ITwitterService.cs
@@ -0,0 +1,14 @@
+﻿using M07_TP04.Entities;
+using M07_TP04.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M07_TP04.Services
+{
+    interface ITwitterService
+    {
+        Boolean authenticate(User user);
+        List<Tweet> getTweets(User user);
+    }
+}
