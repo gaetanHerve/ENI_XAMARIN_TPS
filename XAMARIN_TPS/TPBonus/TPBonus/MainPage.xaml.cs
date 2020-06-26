5818dce465b524f61b52dfed0e7d23816ccf9ea2
Gaëtan <gaetan.herve@lilo.org>
2020-06-26 10:13:06 +0200

    TPBonus initialization

---
+++ XAMARIN_TPS/TPBonus/TPBonus/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TPBonus.Models;
+
+namespace TPBonus
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            new LoginForm(this.login, this.password, this.isRemind, this.loginForm, this.tweetForm, this.errorLabel, this.connection);
+        }
+    }
+}
---
+++ XAMARIN_TPS/TPBonus/TPBonus/Services/ITwitterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TPBonus.Entities;
+
+namespace TPBonus.Services
+{
+    public interface ITwitterService
+    {
+        Boolean Authenticate(User user);
+        List<Tweet> Tweets { get; }
+    }
+}
